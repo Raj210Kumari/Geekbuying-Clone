f650fe4a53f8b15dee65590cffd8298484c1f609
Rajnandani Kumari <50689413+Raj210Kumari@users.noreply.github.com>
2022-11-11 02:18:04 +0530

    day-3

--- electrohippo/src/index.js
+++ electrohippo/src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { AuthContextProvider } from "./Context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <AuthContextProvider>
     <BrowserRouter>
-      <App />
+      <ChakraProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ChakraProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </AuthContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
